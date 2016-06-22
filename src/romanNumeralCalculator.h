91de9508b0c905a6bb1fc11800538000b6833001
staljbs <anarang0920@gmail.com>
2016-06-22 18:06:20 -0400

    Updated README

--- src/romanNumeralCalculator.h
+++ src/romanNumeralCalculator.h
@@ -17,7 +17,7 @@ extern const int INVALID_INPUT;
 extern const char ADDITION_OPERATOR;
 extern const char SUBTRACTION_OPERATOR;
 
-int singleRomanToInt(const char romanNumChar);
+int singleRomanToInt( const char romanNumChar );
 int romanToInt( const char* romanNumStr);
 char* romanAddition(const char* romanNum1, const char* romanNum2);
 char* romanSubtraction(const char* romanNum1, const char* romanNum2);
