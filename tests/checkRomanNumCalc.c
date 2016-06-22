f67572c1c883a41e38decd04f65b6acdfa0c7ff2
staljbs <anarang0920@gmail.com>
2016-06-22 19:12:59 -0400

    Overall Testing Completed

--- tests/checkRomanNumCalc.c
+++ tests/checkRomanNumCalc.c
@@ -324,7 +324,7 @@ int main(void)
 
 	s = romanNumCalculator_suite();
 	sr = srunner_create(s);
-	srunner_set_log(sr, "log/test_romanAddition.log");
+	srunner_set_log(sr, "log/test_overall.log");
 	srunner_run_all(sr, CK_VERBOSE);
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
