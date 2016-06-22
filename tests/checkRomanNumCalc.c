0a8d846a94be69a6e2345d507d0e169d396f0cd1
staljbs <anarang0920@gmail.com>
2016-06-22 14:07:40 -0400

    Added a log folder

--- tests/checkRomanNumCalc.c
+++ tests/checkRomanNumCalc.c
@@ -66,7 +66,7 @@ int main(void)
 
 	s = romanNumCalculator_suite();
 	sr = srunner_create(s);
-	srunner_set_log(sr, "test_romanNumConstants.log");
+	srunner_set_log(sr, "log/test_romanNumConstants.log");
 	srunner_run_all(sr, CK_VERBOSE);
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
